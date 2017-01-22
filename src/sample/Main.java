d13d2371fdee18b0e15b330a069f7d36fa245d75
kritzware <suspiciouspotato@gmail.com>
2017-01-22 19:27:41 +0000

    Changed a few basic settings.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("YouGit");
+        primaryStage.setScene(new Scene(root, 1366, 768));
         primaryStage.show();
     }
 
