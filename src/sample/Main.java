d083b4e5ab516df7144eeed9a6e367eb3a79d83a
kritzware <suspiciouspotato@gmail.com>
2017-01-28 14:44:17 +0000

    Added JGit and JUnit libraries. Initial testing with JGit methods.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,17 +5,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.rules.TemporaryFolder;
+import static org.eclipse.jgit.api.Git.cloneRepository;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception, IOException, GitAPIException {
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("YouGit");
         primaryStage.setScene(new Scene(root, 1366, 768));
         primaryStage.show();
-    }
 
+        File file = new File("test.txt");
+        file.createNewFile();
+        Git git = Git.init().setDirectory(file.getParentFile()).call();
+    }
 
     public static void main(String[] args) {
         launch(args);
