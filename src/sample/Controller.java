d537c1c7a89231cad5040ec84a294a64fdee853a
David Balkwill <JustDave@Davids-MBP-2.eduroam.student>
2017-01-31 15:02:16 +0000

    Test 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
 public class Controller {
+
+
+    public void loginButtonClicked() {
+        System.out.println("Create New");
+
+
+    }
+
+    public void CutButton(){
+
+        System.out.println("Create Cut");
+
+    }
+
+    public void CopyButton(){
+
+        System.out.println("Copy...");
+
+    }
+
+    public void PasteButton(){
+
+        System.out.println("Paste function");
+
+    }
+
+    public void ExitButton(){
+
+    }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,43 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("YouGit");
-        primaryStage.setScene(new Scene(root, 1366, 768));
-        primaryStage.show();
-    }
+    Stage window;
+    Button button;
 
 
     public static void main(String[] args) {
         launch(args);
+
     }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("YouGit");
+
+        window = primaryStage;
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram();
+        });
+
+
+        Scene scene = new Scene(root, 800, 550);
+
+        window.setScene(scene);
+        window.show();
+
+    }
+
+
+    private void closeProgram(){
+       Boolean answer = ConfirmBox.display("Title", "Sure you want to exit?");
+       if(answer)
+           window.close();
+
+    }
+
 }
