3e85773c47cf34b496cd9579b41d331e2d95a83d
Kritzware <suspiciouspotato@gmail.com>
2017-02-06 21:34:24 +0000

    Added pre/post test methods for cleanup. Added init new repo test.

--- src/tests/RepositoryTest.java
+++ src/tests/RepositoryTest.java
@@ -1,16 +1,12 @@
 package tests;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import static junit.framework.TestCase.fail;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import org.junit.*;
 import sample.Repository;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Louis on 06/02/2017.
@@ -18,10 +14,31 @@ import java.io.File;
 
 public class RepositoryTest {
 
-    @Test
-    public void initRepositoryNew() {
-        Repository repository = new Repository("unitTest");
+    private Repository testRepo;
 
+    @Test(expected = Error.class)
+    public void initRepositoryNew() {
+        testRepo = new Repository("unitTest");
+        File newRepoBase = new File(FileSystemView
+                .getFileSystemView()
+                .getDefaultDirectory()
+                .getPath()
+                + "/YouGitRepos/unitTest/");
+        File newRepoGit = new File(FileSystemView
+                .getFileSystemView()
+                .getDefaultDirectory()
+                .getPath()
+                + "/YouGitRepos/unitTest/.git/");
+
+        assertTrue("it should create a local repository", newRepoBase.exists());
+        assertTrue("it should contain a .git folder", newRepoGit.exists());
+
+        try {
+            Repository dupe = new Repository("unitTest");
+            Assert.fail("it should throw an error if the repository already exists");
+        } catch (Exception err) {
+            System.out.println(err.getMessage());
+        }
     }
 
     @Test(expected = Error.class)
@@ -47,5 +64,59 @@ public class RepositoryTest {
         } catch (Exception err) {
             System.out.println(err.getMessage());
         }
+    }
+
+    @BeforeClass
+    public static void preTest() {
+        System.out.println("Deleting test files");
+        File repoTest = new File(FileSystemView
+                .getFileSystemView()
+                .getDefaultDirectory()
+                .getPath()
+                + "/YouGitRepos/unitTest");
+        File cloneTest = new File(FileSystemView
+                .getFileSystemView()
+                .getDefaultDirectory()
+                .getPath()
+                + "/YouGitRepos/kritzbot");
+        deleteFolder(repoTest);
+        deleteFolder(cloneTest);
+
+        /* If some files weren't deleted, try again */
+        if(cloneTest.exists()) {
+            deleteFolder(cloneTest);
+        }
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        System.out.println("Deleting test files");
+        File repoTest = new File(FileSystemView
+                .getFileSystemView()
+                .getDefaultDirectory()
+                .getPath()
+                + "/YouGitRepos/unitTest");
+        File cloneTest = new File(FileSystemView
+                .getFileSystemView()
+                .getDefaultDirectory()
+                .getPath()
+                + "/YouGitRepos/kritzbot");
+        deleteFolder(repoTest);
+        deleteFolder(cloneTest);
+
+        /* If some files weren't deleted, try again */
+        if(cloneTest.exists()) {
+            deleteFolder(cloneTest);
+        }
+    }
+
+    public static void deleteFolder(File dir) {
+        File[] contents = dir.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                deleteFolder(f);
+            }
+        }
+        dir.delete();
     }
 }
